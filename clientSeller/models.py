645c4ac45bdd5f3349182bbba14a99524f7f9e5b
piotrsrednickiDS360 <piotrsrednicki@ds360.pl>
2023-04-25 23:21:44 +0200

    setting up models

--- clientSeller/models.py
+++ clientSeller/models.py
@@ -1,3 +1,37 @@
+from datetime import datetime
+from django.conf import settings
+
 from django.db import models
 
+
 # Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    price = models.DecimalField(decimal_places=2,max_digits=5)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    picture = models.ImageField()
+    miniaturePicture = models.ImageField()
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    client = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    productList = models.JSONField()
+    date = models.DateTimeField(default=datetime.today())
+    paymentDate = models.DateTimeField()
+    price = models.DecimalField(decimal_places=2,max_digits=5)
+
+    def __str__(self):
+        return self.productList.__str__()
