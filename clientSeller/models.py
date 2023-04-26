4b6dd3abb8f91f1750f6bf3e41561c7f30985e98
piotrsrednickiDS360 <piotrsrednicki@ds360.pl>
2023-04-26 19:56:10 +0200

    Changing models format

--- clientSeller/models.py
+++ clientSeller/models.py
@@ -1,22 +1,19 @@
 from datetime import datetime
 from django.conf import settings
-
 from django.db import models
 
 
-# Create your models here.
-
 class ProductCategory(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, primary_key=True)
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, primary_key=True)
     description = models.CharField(max_length=300)
-    price = models.DecimalField(decimal_places=2,max_digits=5)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     picture = models.ImageField()
     miniaturePicture = models.ImageField()
@@ -28,10 +25,10 @@ class Product(models.Model):
 class Order(models.Model):
     client = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     address = models.CharField(max_length=100)
-    productList = models.JSONField()
+    products = models.ManyToManyField(Product)
     date = models.DateTimeField(default=datetime.today())
     paymentDate = models.DateTimeField()
-    price = models.DecimalField(decimal_places=2,max_digits=5)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
 
     def __str__(self):
-        return self.productList.__str__()
+        return self.products.__str__()
