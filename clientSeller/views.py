578414db2fd74a68c1c1ceaf48b8a64b677b5990
piotrsrednickiDS360 <piotrsrednicki@ds360.pl>
2023-04-28 17:46:07 +0200

    Adding permissions for users and pagination

--- clientSeller/views.py
+++ clientSeller/views.py
@@ -1,27 +1,49 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from rest_framework.parsers import JSONParser
+from django.http import HttpResponse
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.views import APIView
-
-from .models import *
 from .serializers import *
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework import generics
-from rest_framework import mixins
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import BasePermission
 
 
-# Create your views here.
+class ListPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+
+class UserRolePermission(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated and request.user.role == 'user':
+            return True
+        else:
+            return False
+
+
+class SellerRolePermission(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated and request.user.role == 'seller':
+            return True
+        else:
+            return False
 
 
 class AllModelsAPIView(APIView):
     authentication_classes = [SessionAuthentication, BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    pagination_class = ListPagination
     currentModel = None
     currentSerializer = None
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            permission_classes = []
+        elif self.request.method == 'POST':
+            permission_classes = [SellerRolePermission]
+        else:
+            permission_classes = [SellerRolePermission]
+        return [permission() for permission in permission_classes]
 
     def __init__(self, currentModel, currentSerializer):
         super().__init__()
@@ -42,8 +64,21 @@ class AllModelsAPIView(APIView):
 
 
 class AllModelsDetailsAPIView(APIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    pagination_class = ListPagination
     currentModel = None
     currentSerializer = None
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            permission_classes = []
+        elif self.request.method == 'PUT':
+            permission_classes = [SellerRolePermission]
+        elif self.request.method == 'DELETE':
+            permission_classes = [SellerRolePermission]
+        else:
+            permission_classes = [SellerRolePermission]
+        return [permission() for permission in permission_classes]
 
     def __init__(self, currentModel, currentSerializer):
         super().__init__()
@@ -60,16 +95,16 @@ class AllModelsDetailsAPIView(APIView):
         except self.currentModel.DoesNotExist:
             return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
-    def get(self, request, name):
-        currentObject = self.get_object(name)
+    def get(self, request, pk):
+        currentObject = self.get_object(pk)
         serializer = self.currentSerializer(currentObject)
         try:
             return Response(serializer.data)
         except AttributeError:
             return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
-    def put(self, request, name):
-        currentObject = self.get_object(name)
+    def put(self, request, pk):
+        currentObject = self.get_object(pk)
         serializer = self.currentSerializer(currentObject, data=request.data)
         try:
             if serializer.is_valid():
@@ -80,121 +115,10 @@ class AllModelsDetailsAPIView(APIView):
         except AttributeError:
             return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
-    def delete(self, request, name):
+    def delete(self, request, pk):
         try:
-            currentObject = self.get_object(name)
+            currentObject = self.get_object(pk)
             currentObject.delete()
             return HttpResponse(status=status.HTTP_204_NO_CONTENT)
         except AttributeError:
             return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-
-
-@api_view(['GET', 'POST'])
-def product_category_list(request):
-    if request.method == 'GET':
-        productCategories = ProductCategory.objects.all()
-        serializer = ProductCategorySerializer(productCategories, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = ProductCategorySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET', 'POST'])
-def product_list(request):
-    if request.method == 'GET':
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET', 'POST'])
-def order_list(request):
-    if request.method == 'GET':
-        orders = Order.objects.all()
-        serializer = OrderSerializer(orders, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = OrderSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def product_category_detail(request, name: str):
-    try:
-        productCategory = ProductCategory.objects.get(name=name)
-
-    except ProductCategory.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = ProductCategorySerializer(productCategory)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = ProductCategorySerializer(productCategory, data=request.data)
-        if serializer.is_valid():
-            productCategory.delete()
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-    elif request.method == 'DELETE':
-        productCategory.delete()
-        return HttpResponse(status=204)
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def product_detail(request, name: str):
-    try:
-        product = Product.objects.get(name=name)
-
-    except Product.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = ProductSerializer(product, data=request.data)
-        if serializer.is_valid():
-            product.delete()
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        product.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
-
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def order_detail(request, pk: int):
-    try:
-        order = Order.objects.get(pk=pk)
-
-    except Order.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = OrderSerializer(order)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = OrderSerializer(order, data=request.data)
-        if serializer.is_valid():
-            order.delete()
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        order.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
