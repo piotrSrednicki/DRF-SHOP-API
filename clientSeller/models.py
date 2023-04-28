d0ce534641a541d0b52e20ed988434e31b7c34a0
piotrsrednickiDS360 <piotrsrednicki@ds360.pl>
2023-04-28 17:44:22 +0200

    Adding a customuser model as well as adding validation to image size, changing model fields

--- clientSeller/models.py
+++ clientSeller/models.py
@@ -1,6 +1,21 @@
+from django.core import validators
+from django.core.exceptions import ValidationError
 from datetime import datetime
 from django.conf import settings
 from django.db import models
+from PIL import Image
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import Group
+
+def validate_image_size(image):
+    img = Image.open(image)
+    width, height = img.size
+    if width > 200 or height > 200:
+        raise ValidationError(f"Max image size is 200x200. The uploaded picture is {width}x{height}.")
+
+
+class CustomUser(AbstractUser):
+    role = models.CharField(max_length=50)
 
 
 class ProductCategory(models.Model):
@@ -13,22 +28,25 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
     description = models.CharField(max_length=300)
-    price = models.DecimalField(decimal_places=2, max_digits=5)
+    price = models.DecimalField(decimal_places=2, max_digits=7)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
-    picture = models.ImageField()
-    miniaturePicture = models.ImageField()
+    picture = models.ImageField(upload_to='pictures/')
+    miniaturePicture = models.ImageField(upload_to='miniatures/', validators=[validate_image_size], null=True,
+                                         editable=False)
 
     def __str__(self):
         return self.name
 
 
 class Order(models.Model):
-    client = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    client = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     address = models.CharField(max_length=100)
     products = models.ManyToManyField(Product)
-    date = models.DateTimeField(default=datetime.today())
+    productsCounts = models.CharField(max_length=100, validators=[validators.validate_comma_separated_integer_list],
+                                      null=True)
+    date = models.DateTimeField(default=datetime.today)
     paymentDate = models.DateTimeField()
-    price = models.DecimalField(decimal_places=2, max_digits=5)
+    price = models.DecimalField(decimal_places=2, max_digits=7, null=True, editable=False)
 
     def __str__(self):
-        return self.products.__str__()
+        return self.client.__str__() + " " + str(self.price)
